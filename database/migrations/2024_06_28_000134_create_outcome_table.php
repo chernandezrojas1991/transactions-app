f283fdd983565d864b26e622b718886db387c163
Carlos Hernández <chernandezrojas1991@gmail.com>
2024-06-28 07:53:57 -0400

    init project
    first commit
    add controller
    add models
    add migrations

---
+++ database/migrations/2024_06_28_000134_create_outcome_table.php
@@ -0,0 +1,27 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('income', function (Blueprint $table) {
+            $table->id();
+            $table->string('reason', 45);
+            $table->unsignedBigInteger('username_fk');
+            $table->foreign('username_fk')->references('username')->on('user')->onDelete('no action');
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('income');
+    }
+};
